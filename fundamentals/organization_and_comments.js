b7e0d35f242a38b7dbbf925fad220ba1db8046cb
Carlos Veras <pessoal.carlosveras@gmail.com>
2022-10-20 08:08:46 -0300

    Start of the course at 19/10/2022

---
+++ fundamentals/organization_and_comments.js
@@ -0,0 +1,22 @@
+//How a complex or a simple script is organized?
+
+//Sentences of code
+console.log("Sentence of code")
+
+//----------------------------------
+
+//Blocks of code
+{
+    {
+        console.log("Sentence in the inner block of code")
+    }
+
+    console.log("Sentence in the outer block of code")
+
+}
+
+//Comment
+
+/*
+    Block of comment
+*/
---
+++ fundamentals/variables_constants.js
@@ -0,0 +1,17 @@
+//var ==> declaring a variable
+var variable = 5
+
+//let ==> declaring a variable
+//Suposedly to be more modern and basically better
+let variablelet = 40
+
+console.log(variable, variablelet)
+
+//Changing them
+
+variable = 200
+variablelet = 300
+
+//Constants
+const constant = 5
+console.log(constant)

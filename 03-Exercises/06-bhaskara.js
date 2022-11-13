7c23a6aa69e454181a96999368f234974869aed8
Carlos Veras <pessoal.carlosveras@gmail.com>
2022-11-13 09:51:14 -0300

    Other 2 exercises

---
+++ 03-Exercises/06-bhaskara.js
@@ -0,0 +1,24 @@
+function printout(input) {
+    console.log(input)
+}
+
+const bhaskara = (ax2 = "ax²", bx = "bx", c = "c") => {
+    a = Number(ax2.replace("x²", ""))
+    b = Number(bx.replace("x", ""))
+    c = Number(c)
+
+    printout(a)
+    printout(b)
+    printout(c)
+
+    delta = b*b - 4 * a * c
+
+    if (delta < 0){
+        printout("Delta is negative")
+        return
+    }
+
+    return [ (-b + Math.sqrt(delta))/(2*a), (-b - Math.sqrt(delta))/(2*a)]
+}
+
+printout(bhaskara("3x²", "-4x", "-5"))
---
+++ 03-Exercises/07-basketball.js
@@ -0,0 +1,26 @@
+const recordes = (points = "") => {
+    let points_v = points.split(" ").map(Number)
+
+    //console.log(points_v)
+
+    let greater = points_v[0]
+    let less = points_v[0]
+
+    let gqtd = 0
+    let lessId = 0
+
+    for (let i = 1; i < points_v.length; i++){
+        if (points_v[i] > greater){
+            gqtd++
+            greater = points_v[i]
+        }
+        if (points_v[i] <= less){
+            less = points_v[i]
+            lessId = i
+        }
+    }
+
+    return [gqtd, lessId + 1]
+}
+
+console.log(recordes("10 20 20 8 25 3 0 30 1"))
